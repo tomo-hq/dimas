e95face06d7157506d3ea9f2d9e73c711b558582
Renato Monroy <rdevelops32@gmail.com>
2025-06-30 20:04:15 -0500

    rm trash

--- src/env.d.ts
+++
@@ -1,9 +0,0 @@
-// Custom types declarations
-declare var myString: string;
-
-declare namespace App {
-  interface Locals {}
-}
-
-// Astro types, not necessary if you already have a `tsconfig.json`
-/// <reference path="../.astro/types.d.ts" />
--- src/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { defineMiddleware } from "astro:middleware";
-
-export const onRequest = defineMiddleware(async (context, next) => {
-  const response = await next();
-  if (response.status === 404) {
-    return context.rewrite("/");
-  }
-  return response;
-});
